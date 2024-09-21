a97c7aa6651fc232d8090bdf663eab3c0c14a563
Omcodes23 <omvataliya23@gmail.com>
2024-09-21 15:57:19 +0530

    checking

--- src/lib/data/certificate.ts
+++ src/lib/data/certificate.ts
@@ -17,7 +17,7 @@ export const certificates: Certificate[] = [
         issuer: "ISRO",
         date: new Date("2024-09-03"),
         description: "Comprehensive course covering HTML, CSS, and JavaScript basics.",
-        image: "/certificates/ISRO.png",
+        image: "/logos/ISRO.png",
         link: "https://onedrive.live.com/?authkey=%21ABuJ%2Dm25beq0iZY&id=9758D34EE6005BC3%211064&cid=9758D34EE6005BC3&parId=root&parQt=sharedby&o=OneUp"
     },
     {
@@ -25,7 +25,7 @@ export const certificates: Certificate[] = [
         issuer: "Department of Computer Applications | MSU",
         date: new Date("2024-02-24"),
         description: "In-depth course on using Python for data analysis and visualization.",
-        image: "/certificates/openhouse2024.png",
+        image: "/logos/openhouse2024.png",
         link: "https://onedrive.live.com/?authkey=%21AKR%2DGlYKhRJfELM&id=9758D34EE6005BC3%21635&cid=9758D34EE6005BC3&parId=root&parQt=sharedby&o=OneUp"
     },
     {
@@ -33,7 +33,7 @@ export const certificates: Certificate[] = [
         issuer: "Postman",
         date: new Date("2023-12-25"),
         description: "Foundational certification for understanding AWS Cloud concepts.",
-        image: "/certificates/postman.png",
+        image: "/logos/postman.png",
         link: "https://onedrive.live.com/?authkey=%21AHrtYHB4ZHfydHo&id=9758D34EE6005BC3%21260&cid=9758D34EE6005BC3&parId=root&parQt=sharedby&o=OneUp"
     },
     {
@@ -41,7 +41,7 @@ export const certificates: Certificate[] = [
         issuer: "International Journal of Intelligent System and Applications in Engineering",
         date: new Date("2024-02-01"),
         description: "Foundational certification for understanding AWS Cloud concepts.",
-        image: "/certificates/ijisae.png",
+        image: "/logos/ijisae.png",
         link: "https://onedrive.live.com/?authkey=%21AMeROmWmQWGL%2D8I&id=9758D34EE6005BC3%21597&cid=9758D34EE6005BC3&parId=root&parQt=sharedby&o=OneUp"
     }
     ,
@@ -50,7 +50,7 @@ export const certificates: Certificate[] = [
         issuer: "Department of Computer Applications | MSU",
         date: new Date("2022-11-08"),
         description: "Foundational certification for understanding AWS Cloud concepts.",
-        image: "/certificates/cs.jpeg",
+        image: "/logos/cs.jpeg",
         link: "https://photos.onedrive.com/share/9758D34EE6005BC3!259?cid=9758D34EE6005BC3&resId=9758D34EE6005BC3!259&authkey=!AIclR1FGv-xXNBo&ithint=photo&e=FL3IX5"
     },
     {
@@ -58,7 +58,7 @@ export const certificates: Certificate[] = [
         issuer: "Department of Computer Applications | MSU",
         date: new Date("2022-02-27"),
         description: "Foundational certification for understanding AWS Cloud concepts.",
-        image: "/certificates/iot.jpeg",
+        image: "/logos/iot.jpeg",
         link: "https://photos.onedrive.com/share/9758D34EE6005BC3!254?cid=9758D34EE6005BC3&resId=9758D34EE6005BC3!254&authkey=!APBF433KLV-dPSw&ithint=photo&e=o82zAv"
     },
     {
@@ -66,7 +66,7 @@ export const certificates: Certificate[] = [
         issuer: "Microsoft",
         date: new Date("2024-01-10"),
         description: "Foundational certification for understanding AWS Cloud concepts.",
-        image: "/certificates/mlsa.png",
+        image: "/logos/mlsa.png",
         link: "https://onedrive.live.com/?authkey=%21ALzTmVlTa0ypXyM&id=9758D34EE6005BC3%21347&cid=9758D34EE6005BC3&parId=root&parQt=sharedby&o=OneUp"
     },
     {
@@ -74,7 +74,7 @@ export const certificates: Certificate[] = [
         issuer: "Institute of Research and Journal",
         date: new Date("2023-10-19"),
         description: "Foundational certification for understanding AWS Cloud concepts.",
-        image: "/certificates/iraj.jpeg",
+        image: "/logos/iraj.jpeg",
         link: "https://photos.onedrive.com/share/9758D34EE6005BC3!256?cid=9758D34EE6005BC3&resId=9758D34EE6005BC3!256&authkey=!AHOdA0u_n8Wcae8&ithint=photo&e=7StywT"
     },
     {
@@ -82,7 +82,7 @@ export const certificates: Certificate[] = [
         issuer: "Department of Computer Applications | MSU",
         date: new Date("2024-07-15"),
         description: "Foundational certification for understanding AWS Cloud concepts.",
-        image: "/certificates/summerintern.jpeg",
+        image: "/logos/summerintern.jpeg",
         link: "https://photos.onedrive.com/share/9758D34EE6005BC3!258?cid=9758D34EE6005BC3&resId=9758D34EE6005BC3!258&authkey=!AHCMyUzfB-Qlztc&ithint=photo&e=shhOuJ"
     }
     
